3b750c1b32e93992835a895f5da7eeb258730ef0
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2023-10-20 11:51:57 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Home, Login } from "@mui/icons-material"
+import { Route, Routes } from "react-router-dom"
 
 
 function App() {
   return (
     <div>
       <h1>admin frontend</h1>
-      
+      <Routes>
+      <Route  path="/" element={<Home/>} />
+      <Route  path="/admin"element={<Login/>} />
+      <Route  path="/signup"element={<SignUp/>} />
+        
+      </Routes>
     </div>
   )
 }
