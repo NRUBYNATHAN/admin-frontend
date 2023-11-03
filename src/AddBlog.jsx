6813c88779e7317bfdf9b4c27044134fe88a6a3f
RUBYNATHAN N <rubynathan999@gmail.com>
2023-11-03 13:25:17 +0530

    title

--- src/AddBlog.jsx
+++ src/AddBlog.jsx
@@ -1,60 +1,70 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import { API } from './global';
-import {useSelector} from "react-redux"
-import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import { API } from "./global";
+import { useSelector } from "react-redux";
+import axios from "axios";
 import "./Blog.css";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export function AddBlog() {
- 
+  const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [blog, setBlog] = useState("");
-  const navigate=useNavigate();
-  const {userInfo} = useSelector((state=>state.auth))
-console.log(userInfo);
-  const handleSubmit= async (e)=>{
+  const navigate = useNavigate();
+  const { userInfo } = useSelector((state) => state.auth);
+  console.log(userInfo);
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-       const newBlog = {
-                user:userInfo._id,
-                username:userInfo.userName,
-                blog: blog,
-                image: image,
-               
-        };
-        console.log(newBlog)
+    const newBlog = {
+      user: userInfo._id,
+      username: userInfo.userName,
+      title: title,
+      blog: blog,
+      image: image,
+    };
+    console.log(newBlog);
 
-      await axios.post(`${API}/api/blog/create`,newBlog)
-      navigate("/")
-       };
+    await axios.post(`${API}/api/blog/create`, newBlog);
+    navigate("/");
+  };
 
-      
   return (
-    <div className='add'>
-    <form  className="input">
-      <h2 className='heading'>Enter Image Url</h2>
-      <TextField className='input1' onChange={(event)=>setImage(event.target.value)}  name ="image" label="image_url" variant="outlined" />
-      <div className="editor">
-                <h2>Write a blog</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello !!</p>"
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        setBlog(data)
-                        
-                    } }
-                   
-                    
-                />
-            </div>
-      <Button className='input1' onClick={handleSubmit}  variant="contained">Add Blog</Button>
-    </form>
+    <div className="add">
+      <form className="input">
+        <TextField
+          className="input1"
+          onChange={(event) => setTitle(event.target.value)}
+          name="title"
+          label="Title"
+          variant="outlined"
+        />
+        <h2 className="heading">Enter Image Url</h2>
+        <TextField
+          className="input1"
+          onChange={(event) => setImage(event.target.value)}
+          name="image"
+          label="image_url"
+          variant="outlined"
+        />
+        <div className="editor">
+          <h2>Write a blog</h2>
+          <CKEditor
+            editor={ClassicEditor}
+            data="<p>Hello !!</p>"
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              setBlog(data);
+            }}
+          />
+        </div>
+        <Button className="input1" onClick={handleSubmit} variant="contained">
+          Add Blog
+        </Button>
+      </form>
     </div>
-   
   );
 }
--- src/EditBlog.jsx
+++ src/EditBlog.jsx
@@ -1,18 +1,18 @@
-import  { useState, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { useNavigate, useParams } from 'react-router-dom';
-import axios from 'axios';
-import { API } from './global';
-import './Blog.css';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { API } from "./global";
+import "./Blog.css";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export function EditBlog() {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const [ruby, setRuby] = useState({ blog: '', image: '' });
+  const [ruby, setRuby] = useState({ title: "", blog: "", image: "" });
 
   useEffect(() => {
     fetch(`${API}/api/blog/${id}`)
@@ -21,7 +21,6 @@ export function EditBlog() {
   }, [id]);
 
   const handleInputChange = (e) => {
-
     const { name, value } = e.target;
     setRuby({ ...ruby, [name]: value });
   };
@@ -30,45 +29,54 @@ export function EditBlog() {
     e.preventDefault();
 
     const updatedBlog = {
+      title: ruby.title,
       blog: ruby.blog,
       image: ruby.image,
     };
 
     await axios.put(`${API}/api/blog/edit/${id}`, updatedBlog);
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <div className='add'>
-    <form onSubmit={handleSubmit} className="input">
-   <h2 className='heading1'>Rewrite a image url</h2>
-      <TextField
-        className="input1"
-        name="image"
-        variant="outlined"
-        
-        value={ruby.image}
-        onChange={handleInputChange}
-      />
-            <div className="editor">
-              <h2>Rewrite a blog summary</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data={`${ruby.blog}`}
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        setRuby({blog:data})
-                        
-                    } }
-                   
-                    
-                />
-            </div>
+    <div className="add">
+      <form onSubmit={handleSubmit} className="input">
+        <TextField
+          className="input1"
+          name="title"
+          variant="outlined"
+          value={ruby.title}
+          onChange={handleInputChange}
+        />
+        <h2 className="heading1">Rewrite a image url</h2>
+        <TextField
+          className="input1"
+          name="image"
+          variant="outlined"
+          value={ruby.image}
+          onChange={handleInputChange}
+        />
+        <div className="editor">
+          <h2>Rewrite a blog summary</h2>
+          <CKEditor
+            editor={ClassicEditor}
+            data={`${ruby.blog}`}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              setRuby({ blog: data });
+            }}
+          />
+        </div>
 
-      <Button className='input1' type="submit" variant="contained" color="success">
-        Save
-      </Button>
-    </form>
-    </div> 
+        <Button
+          className="input1"
+          type="submit"
+          variant="contained"
+          color="success"
+        >
+          Save
+        </Button>
+      </form>
+    </div>
   );
 }
